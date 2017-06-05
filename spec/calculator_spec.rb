a3fc02a97967610102c967be3fc8434394e25904
James Biddle <jpbiddle@hotmail.com>
2017-06-05 09:52:37 -0700

    add testing done

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,11 @@
-# Your code here
+require 'calculator'
+describe Calculator do
+	describe '#add' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.add(1,2)).to eq(3)
+			
+		end
+	  
+	end
+	
+end
