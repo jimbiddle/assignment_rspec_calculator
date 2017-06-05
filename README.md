395e39719839bc6748769917dbfbbdca1534ebf0
James Biddle <jpbiddle@hotmail.com>
2017-06-05 10:57:58 -0700

    most done - issues at end

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 01134
 
 [A Ruby and RSpec testing assignment from the Viking Code School](http://www.vikingcodeschool.com)
-test
+----------------------
+
+Will need some help towards the end. 
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,5 +7,83 @@ describe Calculator do
 		end
 	  
 	end
-	
+
+	describe '#subtract' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.subtract(1,2)).to eq(-1)
+			
+		end
+	end
+
+	describe '#multiply' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.multiply(2,3)).to eq(6)
+		end
+
+	end
+
+	describe '#multiply' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.multiply(2,3)).to eq(6)
+		end
+
+	end
+
+	describe '#divide' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.divide(2,3)).to eq(0.6666666666666666)
+		end
+
+	end
+
+	describe '#divide' do
+		it 'returns the sum of its argument' do
+			expect(Calculator.new.divide(4,2)).to eq(2)
+		end
+
+	end
+
+	describe '#pow' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.pow(3,3)).to eq(27.0)
+		end
+
+	end
+
+	describe '#pow' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.pow(27,1/3.0)).to eq(3.0)
+		end
+
+	end
+
+	describe '#sqrt' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.sqrt(9)).to eq(3)
+		end
+
+	end
+
+	describe '#sqrt' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.sqrt(8)).to eq(2.83)
+		end
+
+	end
+
+
+	describe '#memory' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.memory()).to eq(nil)
+		end
+
+	end
+
+	describe '#memory' do
+		it 'returns the sum of its arguments' do
+			expect(Calculator.new.memory(8)).to eq(nil)
+		end
+
+	end
 end
+
